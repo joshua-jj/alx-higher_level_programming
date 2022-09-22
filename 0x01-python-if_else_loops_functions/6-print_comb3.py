9a92794f9672a3bdae4b00cf092ed938ed0d3c0e
Joshua JJ <akhidenorjoshua@gmail.com>
2022-09-22 21:26:05 +0100

    Add files

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,7 @@ for i in range(10):
     for j in range(10):
         if i < j:
             if i != 8:
-                print('{}'.format(i), end='')
-                print('{}'.format(j), end=', ')
+                print('{}''{}'.format(i, j), end=', ')
         else:
             continue
 print(89)
