fd4fdd4c963fb628343160c11c8bdb57c52d2bb6
Joshua JJ <akhidenorjoshua@gmail.com>
2022-09-22 01:30:01 +0100

    Add file

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+number_str = str(number)
+if(number_str[0] == '-'):
+    print(f'Last digit of {number} is -{number_str[-1]} and is less than 6 and not 0')
+else:
+    if int(number_str[-1]) > 5:
+        print(f'Last digit of {number} is {number_str[-1]} and is greater than 5')
+    elif int(number_str[-1]) == 0:
+        print(f'Last digit of {number} is {number_str[-1]} and is 0')
+    else:
+        print(f'Last digit of {number} is {number_str[-1]} and is less than 6 and not 0')
+    
+
+
---
+++ 0x01-python-if_else_loops_functions/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+import random
+number = random.randint(-10000, 10000)
