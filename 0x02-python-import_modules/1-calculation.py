3db3fb3ca74f3047e74c0f1524cc493da0bf0ff9
Joshua JJ <akhidenorjoshua@gmail.com>
2022-09-24 01:31:53 +0100

    Add files

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,7 @@
+import calculator_1
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
+print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
+print("{} x {} = {}".format(a, b, calculator_1.mul(a, b)))
+print(f"{a} / {b} = {calculator_1.div(a, b)}")
