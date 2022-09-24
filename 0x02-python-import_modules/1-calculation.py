397965226da2418e7313fb29218acf899903298c
Joshua JJ <akhidenorjoshua@gmail.com>
2022-09-24 01:37:07 +0100

    Add files

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
     print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
     print("{} x {} = {}".format(a, b, calculator_1.mul(a, b)))
-    print(f"{a} / {b} = {calculator_1.div(a, b)}")
+    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
