ee247d2d1cd0f444980dc30bf5a3eb3d2d2538d1
Joshua JJ <akhidenorjoshua@gmail.com>
2022-09-25 20:16:01 +0100

    Add files

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3d
-ef new_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
-    if my_list:
-        if idx < 0 or idx > len(my_list) - 1:
-            return new_list
-        new_list[idx] = element
+    if idx < 0 or idx > len(my_list) - 1:
         return new_list
+    new_list[idx] = element
+    return new_list
