4cf70c7ea68e6d07e29723fae945f55a5004c826
Joshua JJ <akhidenorjoshua@gmail.com>
2022-09-25 01:27:03 +0100

    Add files

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > len(my_list) - 1:
+        return None
+    return my_list[idx]
+
+
+print(element_at([2, 4, 2, 6], 4))
