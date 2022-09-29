b355054b5220f149f31f2d7006f78f437ab572af
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2022-09-29 22:43:15 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary, key = lambda k: a_dictionary[k], default = None)
+    if a_dictionary:
+        return max(a_dictionary, key = lambda k: a_dictionary[k])
+    return None
