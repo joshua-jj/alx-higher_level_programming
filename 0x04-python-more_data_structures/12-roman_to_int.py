ef462c5ea47c7f67eaaeb53aa5a92b9703a88058
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2022-10-06 14:08:55 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
     numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                 'C': 100, 'D': 500, 'M': 1000}
@@ -5,9 +6,11 @@ def roman_to_int(roman_string):
     roman_string_list.reverse()
     length = len(roman_string_list)
     sum = numerals[roman_string_list[0]]
-    for i in range(1, length):
-        if numerals[roman_string_list[i]] >= numerals[roman_string_list[i-1]]:
-            sum += numerals[roman_string_list[i]]
-        else:
-            sum -= numerals[roman_string_list[i]]
-    return sum
+    if type(roman_string) == str or roman_string == None:
+        for i in range(1, length):
+            if numerals[roman_string_list[i]] >= numerals[roman_string_list[i-1]]:
+                sum += numerals[roman_string_list[i]]
+            else:
+                sum -= numerals[roman_string_list[i]]
+        return sum
+    return 0
