a791e23d3630d8acf8aab8e8cf20ffcdb7d30dde
Joshua JJ <akhidenorjoshua@gmail.com>
2022-10-06 14:19:10 +0100

    Add files

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                'C': 100, 'D': 500, 'M': 1000}
+    dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000}
     roman_string_list = list(roman_string.upper())
     roman_string_list.reverse()
     length = len(roman_string_list)
-    sum = numerals[roman_string_list[0]]
-    if type(roman_string) == str or roman_string == None:
+    sum = dict[roman_string_list[0]]
+    if roman_string:
         for i in range(1, length):
-            if numerals[roman_string_list[i]] >= numerals[roman_string_list[i-1]]:
-                sum += numerals[roman_string_list[i]]
+            if dict[roman_string_list[i]] >= dict[roman_string_list[i-1]]:
+                sum += dict[roman_string_list[i]]
             else:
-                sum -= numerals[roman_string_list[i]]
+                sum -= dict[roman_string_list[i]]
         return sum
     return 0
