d9896c77cb6eda8913920ca98e166bd9862e0275
Joshua JJ <akhidenorjoshua@gmail.com>
2022-10-06 13:55:07 +0100

    Add files

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,14 @@
-#!/usr/bin/python3
-numerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+def roman_to_int(roman_string):
+    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                'C': 100, 'D': 500, 'M': 1000}
+    roman_string_list = list(roman_string.upper())
+    roman_string_list.reverse()
+    letter = 'V'
+    length = len(roman_string_list)
+    sum = numerals[roman_string_list[0]]
+    for i in range(1, length):
+        if numerals[roman_string_list[i]] >= numerals[roman_string_list[i-1]]:
+            sum += numerals[roman_string_list[i]]
+        else:
+            sum -= numerals[roman_string_list[i]]
+    return sum
---
+++ 0x04-python-more_data_structures/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+ # roman_string_list.reverse()
