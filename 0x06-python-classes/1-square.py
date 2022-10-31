da09cfb07a9873cddf3a9c558b44b9304e27a64e
Joshua JJ <49577373+joshua-jj@users.noreply.github.com>
2022-10-31 14:19:36 +0100

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class square"""
+
+
+class Square:
+    """This is a square class"""
+
+    def __init__(self, size):
+        """Constructor of the square class"""
+        self._size = size
